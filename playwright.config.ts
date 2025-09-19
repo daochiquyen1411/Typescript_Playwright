f33c924e3aa578e932cb79335963aaaea4bd10b1
daochiquyen1411 <72498359+daochiquyen1411@users.noreply.github.com>
2025-09-19 09:58:36 +0700

    test yml

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['html', { open: 'never' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -30,16 +30,16 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
+
     launchOptions: {
       args: [
         '--lang=en-US',
       ]
     },
   },
-
+  outputDir: 'test-results/',
   /* Configure projects for major browsers */
   projects: [
 
@@ -86,6 +86,4 @@ export default defineConfig({
   //   url: 'http://localhost:3000',
   //   reuseExistingServer: !process.env.CI,
   // },
-
-  outputDir: 'test-results/',
 });
